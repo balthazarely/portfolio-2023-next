7578cea252943fb448e61ac2a91d3b46f9bef79a
balthazarely <balthazar.elyj@gmail.com>
2023-04-18 13:11:56 -0600

    added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
